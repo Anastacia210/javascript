940d6af3fbc232105497c06a5593b69fa24ee87d
Anastacia Yatsiuk <anastacia210@gmail.com>
2021-07-29 15:15:19 +0300

    Cw6

---
+++ Lesson 6/main6cw.js
@@ -0,0 +1,25 @@
+fetch('https://jsonplaceholder.typicode.com/posts')
+    //     .then(function (response){
+    //     return response.json();
+    // })
+    //     .then(function (value) {
+    //     console.log(value);
+    // });
+    .then (response => response.json())
+    .then(users => {
+        let usersComment = document.getElementsByClassName('comments-box')[0];
+        for (const user of users) {
+            let comUser = document.createElement('p');
+            comUser.innerText = `${user.id} - ${user.body}`;
+            let detailsBtn = document.createElement('button');
+            detailsBtn.innerText = 'comment';
+            detailsBtn.onclick = function () {
+                fetch(`https://jsonplaceholder.typicode.com/posts/${user.body}`)
+                    .then(value => value.json())
+                    .then(value => {
+                        console.log(value);
+                    });
+            };
+            comUser.appendChild(detailsBtn);
+            usersComment.appendChild(comUser);
+        };})
